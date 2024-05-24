ef728e4402831d95eca9322cbf68f22947df8372
Marie Auer <marieauer29@gmail.com>
2024-05-24 15:51:00 +0200

    challenge

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks
   validates :name, presence: true, uniqueness: true
   has_many :movies, through: :bookmarks, dependent: :destroy
+
+  has_one_attached :photo
 end
